24ed778ba566186e8ffd6b8cebd885302942daf4
kevin <zhukewei@zhukeweideMacBook-Pro.local>
2017-10-31 03:49:59 +1100

    Create superuser automatically

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Start Gunicorn processes
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('da', 'admin@example.com', 'admin123')" | python manage.py shell
 echo Starting Gunicorn.
 exec gunicorn elec5620.wsgi:application \
     --bind 0.0.0.0:8000 \
