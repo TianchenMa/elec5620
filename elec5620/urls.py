36477ba5fa9ce90b1b49366ba74d48d5068a6474
zhuhy <bisdickens@126.com>
2017-10-31 16:15:14 +1100

    FINAL v1

--- elec5620/urls.py
+++ elec5620/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^health/', include('health.urls'))
+    url(r'^health/', include('health.urls')),
+    url(r'^', include('health.urls'))
 ]
